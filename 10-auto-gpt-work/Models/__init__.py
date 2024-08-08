d4ebae00fcdee4fadfa90035ec920dc0250f4d67
Clare Zhou <clare.cz@gmail.com>
2024-08-08 08:42:04 +0800

    chore: Remove unused code files

--- 10-auto-gpt-work/Models/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-AGI_BASE=os.getenv("AGI_BASE")
-sys.path.insert(0,os.path.join(AGI_BASE,"00_shared")) 
-# from backend import *
-from environment_import import *
--- 10-auto-gpt-work/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-AGI_BASE=os.getenv("AGI_BASE")
-sys.path.insert(0,os.path.join(AGI_BASE,"00_shared")) 
-sys.path.insert(0,os.path.join(AGI_BASE,"10-auto-gpt-work")) 
-# from backend import *
-from environment_import import *
